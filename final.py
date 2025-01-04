df32eefb189d6d16afed098b26e4741c7114fb6d
NesterDn <nvsmirnov2001@gmail.com>
2025-01-04 18:42:28 +0300

    04/01/25

--- final.py
+++ final.py
@@ -7,12 +7,8 @@ for i in range(3):
     title = input(f"Введите заголовок заметки {i + 1}: ")
     note["titles"].append(title)
 
-note["contents"] = []
-
-for i in range(3):
-    content = input(f"Введите описание заметки {i + 1}: ")
-
-note["status"] = input("Введите статус заметки: ")
+note["content"] = input(f"Введите описание заметки: ")
+note["status"] = input("Введите статус заметки (Например 'Активна', 'Выполнена'): ")
 note["created_date"] = input("Введите дату создания заметки в формате дд.мм.гггг: ")
 note["issue_date"] = input("Введите дату истечения заметки в формате дд.мм.гггг: ")
 
