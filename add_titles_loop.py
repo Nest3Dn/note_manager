ed507ae2c9d30e201ee5cd473fd57a04da7b758e
NesterDn <nvsmirnov2001@gmail.com>
2025-01-16 16:07:21 +0300

    16.01.2025

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -2,9 +2,9 @@ titles = []
 
 while True:
     user_input = input("Введите заголовок: ")
-    if user_input == "":
+    if user_input == "": #Если пользователь нажимает Enter и ничего не ввводит, цикл останавливается
         break
-    elif user_input in titles:
+    elif user_input in titles: #Если пользователь вводит значение, которое уже вводил ранее, указываем на это
         print("Такой заголовок уже есть")
     else:
-        titles.append(user_input)
+        titles.append(user_input) #Добавляем в список данные пользователя
