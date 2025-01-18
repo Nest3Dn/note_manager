337900ba822ec09fea60bac16195df23e40fa61b
NesterDn <nvsmirnov2001@gmail.com>
2025-01-18 12:18:28 +0300

    18.01.2025

---
+++ delete_note.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+
+print("Добро пожаловать в Менеджер заметок! Вы можете добавить новую заметку.")
+
+
+def create_note():
+    name = input("Введите имя пользователя: ")
+    title = input("Введите заголовок заметки: ")
+    content = input("Введите описание заметки: ")
+    status = input("Введите статус заметки: ")
+
+    while True:
+        create_date = input("Введите дату создания заметки в формате (день-месяц-год): ")
+        try:
+            create_date = datetime.strptime(create_date, "%d-%m-%Y")
+            formatted1 = create_date.strftime("%d-%m-%Y")
+            break
+        except ValueError:
+            print("Произошла ошибка! Пожалуйста, введите дату в формате (день-месяц-год).")
+
+    while True:
+        issue_date = input("Введите дедлайн заметки в формате (день-месяц-год): ")
+        try:
+            deadline = datetime.strptime(issue_date, "%d-%m-%Y")
+            formatted2 = deadline.strftime("%d-%m-%Y")
+            break
+        except ValueError:
+            print("Произошла ошибка! Пожалуйста, введите дату в формате (день-месяц-год).")
+
+    return {
+        "Имя": name,
+        "Заголовок": title,
+        "Описание": content,
+        "Статус": status,
+        "Дата создания": formatted1,
+        "Дедлайн": formatted2
+    }
+
+
+def display_notes(notes):
+    if not notes:
+        print("Нет сохраненных заметок.")
+        return
+
+    for i, note in enumerate(notes):
+        print(f"Заметка #{i + 1}")
+        for key, value in note.items():
+            print(f"  {key}: {value}")
+
+
+def delete_note(notes):
+
+    if not notes:
+        print("Нет заметок для удаления.")
+        return notes
+
+    search_term = input("Введите имя или заголовок заметки для удаления: ")
+
+    notes_to_remove = []
+    for note in notes:
+        if note["Имя"].lower() == search_term.lower() or note["Заголовок"].lower() == search_term.lower():
+            notes_to_remove.append(note)
+
+    if not notes_to_remove:
+        print("Заметка не найдена.")
+    else:
+        for note in notes_to_remove:
+            notes.remove(note)
+        print(f"Удалено заметок: {len(notes_to_remove)} ")
+    return notes
+
+
+def main():
+    notes = []
+    while True:
+        command = input("Введите 'создать' для добавления, 'удалить' для удаления или 'стоп' для завершения: ").lower()
+
+        if command == "создать":
+            new_note = create_note()
+            notes.append(new_note)
+            print("Заметка добавлена.")
+        elif command == "удалить":
+            notes = delete_note(notes)
+        elif command == "стоп":
+            break
+        else:
+            print("Неверная команда, попробуйте еще раз.")
+
+    display_notes(notes)
+
+
+if __name__ == "__main__":
+    main()
